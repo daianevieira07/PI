35f5bc4c684ee712851d5b65216ef4ad98a673e4
Daiane Vieira <daiane.vieira07@yahoo.com.br>
2024-05-16 21:49:52 -0300

    Ajustando Layout do Form e ajustes de altura das páginas.

--- spa.js
+++ spa.js
@@ -57,7 +57,7 @@ function getPageContent(page) {
 }
  */
 
-var pages = {
+/* var pages = {
   home: `<div style="position: relative; width: 100%; height: -5; padding-top: 56.2225%;
         padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
         border-radius: 8px; will-change: transform;">
@@ -77,6 +77,57 @@ var pages = {
     border-radius: 8px; will-change: transform;">
      <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
        src="https://www.canva.com/design/DAGFOY9xjGs/G2S9FR0MV1NssSP3yomniw/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+     </iframe>
+   </div>`,
+  contact: `<div class="background">
+   <form class="form">
+     <label for="name">Nome:</label><br>
+     <input type="text" class="inputUser" placeholder="Nome " required> </br>
+     <label for="telefone">Telefone </label><br>
+     <input type="telefone" id="telefone" placeholder="Telefone" required></br>
+     <label for="email">E-mail:</label><br>
+     <input type="email" id="email" placeholder="Email" required></br>
+     <label for="message">Mensagem:</label><br>
+     <textarea id="message" name="message" placeholder="Deixe aqui sua mensagem" required></textarea><br><br>
+     <input type="submit" value="Enviar">
+   </form>
+   </div>`,
+};
+
+function getPageContent(page) {
+  var contentToReturn;
+  switch (page) {
+    case "home":
+      contentToReturn = pages.home;
+      break;
+    case "aboutus":
+      contentToReturn = pages.aboutus;
+      break;
+    case "services":
+      contentToReturn = pages.services;
+      break;
+    case "contact":
+      contentToReturn = pages.contact;
+      break;
+    default:
+      contentToReturn = pages.home;
+      break;
+  }
+  document.getElementById("content").innerHTML = contentToReturn;
+}
+ */
+
+var pages = {
+  home: `<div class="iframe-container">
+        <iframe loading="lazy" src="https://www.canva.com/design/DAGB3eKYNa0/DCsnBxwSl6bx7EVpunixqw/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+        </iframe>
+      </div>`,
+  aboutus: `<div class="iframe-container">
+     <iframe loading="lazy" src="https://www.canva.com/design/DAGFNEQHupQ/0QzbpsDiA9_36Tz37YhFTQ/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+     </iframe>
+   </div>`,
+  services: `<div class="iframe-container">
+     <iframe loading="lazy" src="https://www.canva.com/design/DAGFOY9xjGs/G2S9FR0MV1NssSP3yomniw/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
      </iframe>
    </div>`,
   contact: `<div class="background">
